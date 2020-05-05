a9b6b11277644a2affff41ab28b77956ba83f282
pep1994 <marcopetrini1994@live.it>
2020-05-05 12:57:21 +0200

    test php - 2

--- php1/index.php
+++ php1/index.php
@@ -1,23 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Test php</title>
 </head>
+
 <body>
 
   <h1>
 
-    <?php 
+    <?php
 
-      $name = $_GET['name'];
-      $mail = $_GET['mail'];
-      $age = $_GET['age'];
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
 
-      echo $name . '<br>' . $mail . "<br>" . $age;
+    if (strlen($name) > 3 && strpos($mail, "@") && strpos($mail, ".") && is_numeric($age)) {
+      echo "Accesso consentito";
+    } else {
+      echo "Accesso negato";
+    }
+
 
     ?></h1>
-  
+
+  <p>
+    <?php
+    echo $name;
+    ?>
+  </p>
+
+  <p>
+    <?php
+    echo $mail;
+    ?>
+  </p>
+
+  <p>
+    <?php
+    echo $age;
+    ?>
+  </p>
+
 </body>
+
 </html>
